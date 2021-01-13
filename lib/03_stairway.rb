dfacc1bbd56b69d4dcfe6b283d7af73e76e2dcd7
Lecogoni <giraud.nicolas@me.com>
2021-01-13 14:47:09 +0100

    ajout method ask_dice et result au fichier 4

--- lib/03_stairway.rb
+++ lib/03_stairway.rb
@@ -1,7 +1,11 @@
 
-def dice
+def ask_dice
     dice = rand(1..6)
     return dice
+end
+
+def result(floor_level)
+    return puts "Tu as obtenue #{ask_dice}. Tu es maintenant au niveau #{floor_level} !"
 end
 
 def stairway_game(dice)
@@ -11,27 +15,29 @@ def stairway_game(dice)
         puts "tu es au niveau #{floor_level}"
         puts "Press enter pour lancer le dé"
         enter = gets.chomp
-        
+        dice = ask_dice
         roll_dice_times += 1
         if dice == 5 || dice == 6
             floor_level += 1 
-            puts "Tu as obtenue #{dice}, tu avances d'une marche. Tu es maintenant au niveau #{floor_level} !"
-            puts " "
+            result(floor_level)
+
         elsif dice == 1
             floor_level -= 1 
-            puts "Tu as obtenue #{dice}, tu recules d'une marche. Tu es maintenant au niveau #{floor_level} !"
-            puts " "
+            result(floor_level)
+ 
         else dice == 2 || dice == 3 || dice == 4
-            puts "Tu as obtenue #{dice}, il ne se passe rien. Tu reste au niveau #{floor_level} !"
-            puts " "
+            result(floor_level)
+
         end
 
     end
     puts " tu as gagné en #{roll_dice_times} lancé de dés !!"
+    return floor_level
 end
 
 def perform
-    stairway_game(dice)
+    stairway_game(ask_dice)
 end
 
 perform
+
